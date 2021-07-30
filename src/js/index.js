e9fed56bfb073ca310c4b737f81dec7170e75381
Bartłomiej Rzęsista <bartek.rzesista@gmail.com>
2021-07-30 22:57:32 +0200

    add Event Listener for input changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import '../scss/index.scss';
 import caesar13 from './caesar';
 
+const input = document.querySelector('.main__input');
+const output = document.querySelector('.main__output');
+
+input.addEventListener('keyup', () => {
+    if(input.value != '') output.value = caesar13(input.value);
+    else output.value = '';
+});
+
 console.log(caesar13('PRZEPROGRAMOWANI'));
